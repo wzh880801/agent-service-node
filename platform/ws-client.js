ea8ae6f145da14dd5fbedea4f9cb475b3623333c
王重 <wangzhong.1@bytedance.com>
2025-03-27 23:58:02 +0800

    add SAVE_RAW_EVENT flag

--- platform/ws-client.js
+++ platform/ws-client.js
@@ -11,7 +11,7 @@ myQueue.on('error', err => {
     console.log(`[ws]bull queue error`, err);
 })
 
-const { APP_ID, APP_SECRET, LOG_LEVEL } = process.env;
+const { APP_ID, APP_SECRET, LOG_LEVEL, SAVE_RAW_EVENT } = process.env;
 const logLevel = LOG_LEVEL && typeof lark.LoggerLevel[LOG_LEVEL] !== 'undefined' ? lark.LoggerLevel[LOG_LEVEL] : lark.LoggerLevel.info;
 
 logger.info(`Using app ${APP_ID} to subscribe event.`);
@@ -60,7 +60,9 @@ wsClient.start({
         'apaas.application.metric.reported_v1': async (data) => {
 
             const _logger = logger.default().new();
-            // _logger.append({ ext: 'APPLICATION_METRICS' }).info(data);
+            if (SAVE_RAW_EVENT) {
+                _logger.append({ ext: 'APPLICATION_METRICS_RAW' }).info(data);
+            }
 
             await myQueue.add('apaas_metrics', {
                 ...data,
@@ -72,6 +74,9 @@ wsClient.start({
         'apaas.application.event.reported_v1': async (data) => {
 
             const _logger = logger.default().new();
+            if (SAVE_RAW_EVENT) {
+                _logger.append({ ext: 'APPLICATION_EVENTS_RAW' }).info(data);
+            }
 
             await myQueue.add('apaas_events', {
                 ...data,
@@ -82,6 +87,9 @@ wsClient.start({
 
         'apaas.application.log.reported_v1': async (data) => {
             const _logger = logger.default().new();
+            if (SAVE_RAW_EVENT) {
+                _logger.append({ ext: 'APPLICATION_LOGS_RAW' }).info(data);
+            }
 
             await myQueue.add('apaas_logs', {
                 ...data,
